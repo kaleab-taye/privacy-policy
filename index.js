6b5b5cbaaec669741572521e2347f228c3187b23
kaleab-taye <kaleabtaye@gmail.com>
2022-07-21 20:17:23 +0300

    update

--- index.js
+++ index.js
@@ -1,22 +1,26 @@
+import cool from 'cool-ascii-faces';
+
 import express from 'express';
 import { page } from './Privacy-policy-page.js';
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app
+  .get('/cool', (req, res) => res.send(cool()))
+  .get('/', (req, res) => {
+    res.send(page());
+  });
 
-app.use('/', (req, res) => {
-    res.send(page())});
 app.use('/:else', (req, res) => {
-  res.json({"error" : {"message":"page not found"}});
+  res.json({ error: { message: 'page not found' } });
 });
 
-app.use((err,req,res,text)=>{
-  console.error(err.stack)
-  res.type('application/json')
-  res.status(500).send({error: "Internal server error"})
-})
-
+app.use((err, req, res, text) => {
+  console.error(err.stack);
+  res.type('application/json');
+  res.status(500).send({ error: 'Internal server error' });
+});
 
 app.listen(PORT, () => {
   console.log(`Server Running on port ${PORT}`);
